426531589ea0da82c3d9bbd785e1a60262ca3b94
Adrian <adrian98goagal@gmail.com>
2019-02-09 21:46:05 +0200

    Main

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,5 +1,17 @@
+import business.WorkoutService;
+import domain.Workout;
+import persistance.DAO;
+import persistance.InMemoryDAO;
+import presentation.ConsoleUI;
+
 public class Main {
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        DAO<String, Workout> workoutDAO = new InMemoryDAO<>();
+        WorkoutService workoutService = new WorkoutService(workoutDAO);
+        ConsoleUI console = new ConsoleUI(workoutService);
+
+        console.run();
     }
 }
